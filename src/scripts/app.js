de1f88651ddfce952373819ab8f686e4f29e9c3c
Denis <d.s.golubtsov@gmail.com>
2016-05-18 23:38:29 +0300

    add anchor for map.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -395,5 +395,30 @@ define('app', [
         });
     })($('.j-scroll-down'));
 
+    // to top right away
+    if ( window.location.hash ) scroll(0,0);
+    // void some browsers issue
+    setTimeout( function() { scroll(0,0); }, 1);
+
+    $(function() {
+        // your current click function
+        $('.j-anchor-scroll').on('click', function(e) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top + 'px'
+            }, 1000, 'swing');
+        });
+
+        // *only* if we have anchor on the url
+        if(window.location.hash) {
+
+            // smooth scroll to the anchor id
+            $('html, body').animate({
+                scrollTop: $(window.location.hash).offset().top + 'px'
+            }, 1000, 'swing');
+        }
+
+    });
+
     return {};
 });
