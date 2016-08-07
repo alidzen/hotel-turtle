e4145cbcbede49cce677241b47ffb891f938c8f5
alidzen <d.s.golubtsov@gmail.com>
2016-08-07 21:03:00 +0300

    New popup like on booking.com. transfer to tpl, + save old template like template_07_08_16, change tpl (first time change wrong templates)

--- ftp/local/templates/.default/components/bitrix/news/rooms/bitrix/news.list/.default/template.php
+++ ftp/local/templates/.default/components/bitrix/news/rooms/bitrix/news.list/.default/template.php
@@ -74,106 +74,96 @@ $isRoomsDir = ($APPLICATION->GetCurDir() == '/rooms/');
 							<div class="l-action__border">
 								<div class="l-action__cnt">
 									<div class="l-action__ttl">
-										<h2 class="f-adaptive-ttl b-bottom-dash-pos-left"><?=$arItem['FIELDS']['NAME'];?></h2>
+										<h2 class="f-callback-note b-bottom-dash-pos-left has_margin-small"><?=$arItem['FIELDS']['NAME'];?></h2>
+										<div class="f-adaptive-txt">Идеально для путешественника
+											<span class="b-price">
+												<? if ($arResult['PROPERTIES']['PRICE_FROM']['VALUE'] != ''): ?>
+													<? if ($arResult['PROPERTIES']['PRICE_TO']['VALUE'] != ''): ?>
+													<?=GetMessage('FROM');?> <?=$arResult['PROPERTIES']['PRICE_FROM']['VALUE'];?> <span
+														class="b-ruble">g</span>
+													<?=GetMessage('TO');?> <?=$arResult['PROPERTIES']['PRICE_TO']['VALUE'];?>  <span
+														class="b-ruble">g</span>
+													<? else: ?>
+													<?=$arResult['PROPERTIES']['PRICE_FROM']['VALUE'];?> <span
+														class="b-ruble">g</span>
+													<? if ($arResult['PROPERTIES']['OLD_PRICE']['VALUE'] != ''): ?>
+													<span
+															class="b-room__old-price"><?=$arResult['PROPERTIES']['OLD_PRICE']['VALUE'];?>
+														<span class="b-ruble">g</span></span>
+													<? endif; ?>
+													<?endif;?>
+												<? endif; ?>
+											</span>
+										</div>
 										<p><?=$arItem['FIELDS']['DETAIL_TEXT'];?></p>
 									</div>
 									<div class="l-room-item">
-										<div class="l-room-item__params l-room-item__params_theme_price">
-											<div class="b-room-param">
-												<div class="b-room-param__txt"><?=GetMessage('PRICE');?></div>
-												<div class="b-room-param__data">
-													<? if ($arItem['PROPERTIES']['PRICE_FROM']['VALUE'] != ''): ?>
-														<? if ($arItem['PROPERTIES']['PRICE_TO']['VALUE'] != ''): ?>
-															<?=GetMessage(
-																'FROM'
-															);?> <?=$arItem['PROPERTIES']['PRICE_FROM']['VALUE'];?><span
-																class="b-ruble">g</span>
-															<?=GetMessage(
-																'TO'
-															);?> <?=$arItem['PROPERTIES']['PRICE_TO']['VALUE'];?>  <span
-																class="b-ruble">g</span>
-														<? else: ?>
-															<?=$arItem['PROPERTIES']['PRICE_FROM']['VALUE'];?> <span
-																class="b-ruble">g</span>
-															<? if ($arItem['PROPERTIES']['OLD_PRICE']['VALUE'] != ''): ?>
-																<span
-																	class="b-room__old-price"><?=$arItem['PROPERTIES']['OLD_PRICE']['VALUE'];?>
-																	<span class="b-ruble">g</span></span>
-															<? endif; ?>
-														<? endif; ?>
-													<? endif; ?>
+										<? if (count($arResult['galery']) > 0): ?>
+										<div class="l-room-item__gallery">
+											<div class="b-gallery b-gallery_theme_room j-gallery j-gallery_theme_mobile">
+												<div data-ratio="16/9" data-width="100%" data-arrows="false" data-nav="thumbs"
+													 data-fit="cover" data-loop="true" data-autoplay="false" data-transition="dissolve"
+													 data-transitionduration="600" data-allowfullscreen="native" data-thumbwidth="100"
+													 data-margin="-1" class="b-gallery__base">
+													<? foreach ($arResult['galery'] as $arImage): ?>
+													<a href="<?=$arImage['SRC']?>"></a>
+													<? endforeach; ?>
 												</div>
-											</div>
-										</div>
-										<? if (count($arItem['galery']) > 0): ?>
-											<div class="l-room-item__gallery">
-												<div class="b-gallery b-gallery_theme_room j-gallery">
-													<div data-height="650" data-width="100%" data-arrows="false" data-nav="thumbs"
-														 data-fit="cover" data-loop="true" data-autoplay="false" data-transition="dissolve"
-														 data-transitionduration="600" data-allowfullscreen="native" data-thumbwidth="100"
-														 data-margin="-1" class="b-gallery__base">
-														<? foreach ($arItem['galery'] as $arImage): ?>
-															<a href="<?=$arImage['SRC']?>"></a>
-														<? endforeach; ?>
-													</div>
-													<a href="javascript:;"
-													   class="j-gallery__prev b-gallery__arrow b-gallery__arrow_show_prev">Показать
-														предыдущий слайд</a>
-													<a href="javascript:;"
-													   class="j-gallery__next b-gallery__arrow b-gallery__arrow_show_next">Показать
-														следущий
-														слайд</a></div>
-											</div>
-										<? endif; ?>
-										<div class="l-room-item__params">
-											<? if ($arItem['PROPERTIES']['R_SIZE']['VALUE'] != ''): ?>
-											<div class="b-room-param">
-												<div class="b-room-param__txt"><?=GetMessage('ROOM_SIZE_TEXT');?></div>
-												<div class="b-room-param__data"><?=$arItem['PROPERTIES']['R_SIZE']['VALUE'];?> м<sup>2</sup></div>
-											</div>
-											<?endif;?>
-											<? if ($arItem['PROPERTIES']['BED']['VALUE'] != ''): ?>
-											<div class="b-room-param">
-												<div class="b-room-param__txt"><?=GetMessage('NUM_OF_BED');?></div>
-												<div class="b-room-param__data"><?=$arItem['PROPERTIES']['BED']['VALUE'];?></div>
-											</div>
-											<?endif;?>
-											<? if ($arItem['PROPERTIES']['P_MAX']['VALUE'] != ''): ?>
-											<div class="b-room-param">
-												<div class="b-room-param__txt"><?=GetMessage('NUM_OF_GUESTS');?></div>
-												<div class="b-room-param__data"><?=$arItem['PROPERTIES']['P_MAX']['VALUE'];?></div>
-											</div>
-											<?endif;?>
-										</div>
-										<div class="l-room__feature">
-											<ul class="b-room-list">
-												<li class="b-room-list__item">
-													<div class="b-room-list__item-img b-room-list__item-img_img_window"></div>
-													<div class="b-room-list__item-txt"><?=GetMessage('MANSARDA');?></div>
-												</li>
-												<li class="b-room-list__item">
-													<div class="b-room-list__item-img b-room-list__item-img_img_fan"></div>
-													<div class="b-room-list__item-txt"><?=GetMessage('COND');?></div>
-												</li>
-												<li class="b-room-list__item">
-													<div class="b-room-list__item-img b-room-list__item-img_img_wi-fi"></div>
-													<div class="b-room-list__item-txt"><?=GetMessage('FREE_WIFI');?></div>
-												</li>
-											</ul>
-										</div>
-										<? if (count($arItem['services'])): ?>
-										<div class="l-room-item__list-row b-typo-reset">
-											<? foreach ($arItem['services'] as $arCol): ?>
-												<div class="l-room-item__list-col">
-													<ul>
-														<? foreach ($arCol as $strSname): ?>
-															<li><?=$strSname;?></li>
-														<? endforeach; ?>
-													</ul>
-												</div>
-											<? endforeach; ?>
+												<a href="javascript:;"
+												   class="j-gallery__prev b-gallery__arrow b-gallery__arrow_show_prev">Показать
+													предыдущий слайд</a>
+												<a href="javascript:;"
+												   class="j-gallery__next b-gallery__arrow b-gallery__arrow_show_next">Показать следущий
+													слайд</a></div>
 										</div>
 										<? endif; ?>
+										<div class="l-room-item__descr">
+											<div class="l-room-item__params">
+												<? if ($arResult['PROPERTIES']['R_SIZE']['VALUE'] != ''): ?>
+												<div class="b-room-param"><?=GetMessage('ROOM_SIZE_TEXT');?>: 
+													<span class="b-room-param__data"><?=$arResult['PROPERTIES']['R_SIZE']['VALUE'];?> м<sup>2</sup></span>
+												</div>
+												<? endif; ?>
+												<? if ($arResult['PROPERTIES']['BED']['VALUE'] != ''): ?>
+												<div class="b-room-param"><?=GetMessage('NUM_OF_BED');?>: 
+													<span class="b-room-param__data"><?=$arResult['PROPERTIES']['BED']['VALUE'];?></span>
+												</div>
+												<? endif; ?>
+												<? if ($arResult['PROPERTIES']['P_MAX']['VALUE'] != ''): ?>
+												<div class="b-room-param"><?=GetMessage('NUM_OF_GUESTS');?>: 
+													<span class="b-room-param__data"><?=$arResult['PROPERTIES']['P_MAX']['VALUE'];?></span>
+												</div>
+												<? endif; ?>
+											</div>
+											<div class="l-room-item__feature">
+												<ul class="b-room-list">
+													<li class="b-room-list__item">
+														<div class="b-room-list__item-img b-room-list__item-img_img_window"></div>
+														<div class="b-room-list__item-txt"><?=GetMessage('MANSARDA');?></div>
+													</li>
+													<li class="b-room-list__item">
+														<div class="b-room-list__item-img b-room-list__item-img_img_fan"></div>
+														<div class="b-room-list__item-txt"><?=GetMessage('COND');?></div>
+													</li>
+													<li class="b-room-list__item">
+														<div class="b-room-list__item-img b-room-list__item-img_img_wi-fi"></div>
+														<div class="b-room-list__item-txt"><?=GetMessage('FREE_WIFI');?></div>
+													</li>
+												</ul>
+											</div>
+											<? if (count($arResult['services'])): ?>
+											<div class="l-room-item__hang">
+												<ul class="b-room-hang">
+													<? foreach ($arCol as $strSname): ?>
+													<li><?=$strSname;?></li>
+													<? endforeach; ?>
+												</ul>
+											</div>
+											<? endif; ?>
+											<div class="l-room-item__btn">
+												<a href="#" class="b-btn b-btn_width_auto">Забронировать</a>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
